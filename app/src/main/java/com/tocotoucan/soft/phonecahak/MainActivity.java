614a2345c23997bfe477be6095a4a7eafbd37fe3
kangilkueon <kangilkueon@gmail.com>
2015-10-28 03:06:54 +0900

    First work, Making intro image

--- app/src/main/java/com/tocotoucan/soft/phonecahak/MainActivity.java
+++ app/src/main/java/com/tocotoucan/soft/phonecahak/MainActivity.java
@@ -1,16 +1,77 @@
 package com.tocotoucan.soft.phonecahak;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    SurfaceView camera_preview;
+    SurfaceHolder holder;
+    Camera mCamera;
+
+    LinearLayout intro_layout;
+    ImageView intro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intro_layout = (LinearLayout) findViewById(R.id.introLayout);
+        intro = (ImageView) findViewById(R.id.introImage);
+        intro.setBackgroundResource(R.drawable.intro);
+        AnimationDrawable frameAnimation = (AnimationDrawable) intro.getBackground();
+        frameAnimation.start();
+
+        boolean isRunIntro = getIntent().getBooleanExtra("intro", true);
+        if(isRunIntro) {
+            beforeIntro();
+        } else {
+            frameAnimation.stop();
+            afterIntro(savedInstanceState);
+        }
+    }
+
+    // 인트로 화면
+    private void beforeIntro() {
+        // 약 2초간 인트로 화면을 출력.
+        getWindow().getDecorView().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("intro", false);
+                startActivity(intent);
+                // 액티비티 이동시 페이드인/아웃 효과를 보여준다. 즉, 인트로
+                //    화면에 부드럽게 사라진다.
+                overridePendingTransition(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+            }
+        }, 3100);
+    }
+
+    // 인트로 화면 이후.
+    private void afterIntro(Bundle savedInstanceState) {
+        intro_layout.setVisibility(View.INVISIBLE);
+        //intro_layout.removeAllViews();
+        camera_preview = (SurfaceView) findViewById(R.id.cameraSurface);
+
+        holder = camera_preview.getHolder();
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        holder.addCallback(surfaceListener);
     }
 
     @Override
@@ -34,4 +95,35 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private SurfaceHolder.Callback surfaceListener = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder){
+            mCamera = Camera.open();
+
+            try {
+                mCamera.setPreviewDisplay(holder);
+            } catch (Exception e){
+                e.printStackTrace();
+                mCamera.release();
+                mCamera = null;
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width,	int height) {
+            Camera.Parameters parameters = mCamera.getParameters();
+            //parameters.setPreviewSize(width, height);
+            //mCamera.setParameters(parameters);
+            mCamera.startPreview();
+        }
+    };
 }
